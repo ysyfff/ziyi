c1f1428a2d33cd1416efdfcb1129afc520c10174
ysy <1156546473@qq.com>
2020-10-28 20:34:09 +0800

    clean service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,148 +5,71 @@ Service Worker是一个标准的 web worker，浏览器用一个单独的线程�
 */
 // configuration
 const
-  version = '1.0.1',
+  version = '1.0.2',
   CACHE = version + '::ZiyiMember',
-  offlineURL = '/offline',
   installFilesEssential = [
     '/',
     '/manifest.json',
-  ].concat(offlineURL),
-  installFilesDesirable = [
     '/favicon.ico',
     '/logo.jpeg',
   ];
 
 // install static assets
 function installStaticFiles() {
-
   return caches.open(CACHE)
     .then(cache => {
-
-      // cache desirable files
-      cache.addAll(installFilesDesirable);
-      // cache essential files
       return cache.addAll(installFilesEssential);
-
     });
-
 }
 
-// application installation
-self.addEventListener('install', event => {
-
-  console.log('service worker: install');
-
-  // cache core files
-  event.waitUntil(
-    installStaticFiles()
-      .then(() => self.skipWaiting())
-  );
-
-});
-
-// clear old caches
 function clearOldCaches() {
-
   return caches.keys()
     .then(keylist => {
-
       return Promise.all(
         keylist
           .filter(key => key !== CACHE)
           .map(key => caches.delete(key))
       );
-
     });
-
 }
 
-// application activated
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    installStaticFiles()
+      .then(() => self.skipWaiting())
+  );
+});
+
 self.addEventListener('activate', event => {
-
-  console.log('service worker: activate');
-
-  // delete old caches
   event.waitUntil(
     clearOldCaches()
       .then(() => self.clients.claim())
   );
-
 });
 
-// application fetch network data
 self.addEventListener('fetch', event => {
-
-  // abandon non-GET requests
   if (event.request.method !== 'GET') return;
-
   let url = event.request.url;
-
   event.respondWith(
-
     caches.open(CACHE)
       .then(cache => {
-
         return cache.match(event.request)
           .then(response => {
-
             if (response) {
-              // return cached file
-              console.log('cache fetch: ' + url);
               return response;
             }
-
-            // make network request
             return fetch(event.request)
               .then(newreq => {
-
                 console.log('network fetch: ' + url);
                 if (newreq.ok) cache.put(event.request, newreq.clone());
                 return newreq;
 
               })
-              // app is offline
-              .catch(() => offlineAsset(url));
-
+              .catch(()=>null);
           });
 
       })
-
   );
-
 });
 
-// is image URL?
-let iExt = ['png', 'jpg', 'jpeg', 'gif', 'webp', 'bmp'].map(f => '.' + f);
-function isImage(url) {
-
-  return iExt.reduce((ret, ext) => ret || url.endsWith(ext), false);
-
-}
-
-
-// return offline asset
-function offlineAsset(url) {
-
-  if (isImage(url)) {
-
-    // return image
-    return new Response(
-      '<svg role="img" viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg"><title>offline</title><path d="M0 0h400v300H0z" fill="#eee" /><text x="200" y="150" text-anchor="middle" dominant-baseline="middle" font-family="sans-serif" font-size="50" fill="#ccc">offline</text></svg>',
-      {
-        headers: {
-          'Content-Type': 'image/svg+xml',
-          'Cache-Control': 'no-store'
-        }
-      }
-    );
-
-  }
-  else {
-
-    // return page
-    return caches.match(offlineURL);
-
-  }
-
-}
