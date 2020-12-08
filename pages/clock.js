640c2a9701c9b3ec933c9caee792167db878dce1
ysy <1156546473@qq.com>
2020-12-08 17:04:42 +0800

    style

--- pages/clock.js
+++ pages/clock.js
@@ -8,8 +8,10 @@ import '../styles/clock.less'
 const AutoplaySlider = withAutoplay(AwesomeSlider)
 
 const imgs = [
-  "/smile.jpeg",
-  "/clothes.jpeg",
+  "https://up.enterdesk.com/edpic_360_360/27/8f/93/278f938be4b460a57962d542eee989f6.jpg",
+  "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2534506313,1688529724&fm=26&gp=0.jpg",
+  // "/smile.jpeg",
+  // "/clothes.jpeg",
 ];
 
 const fix0 = (str) => {
