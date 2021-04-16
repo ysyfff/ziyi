274dc0ce81e550771fa04e75e84b7cea0470ceba
ysy尹士勇 <shiyongyin@trip.com>
2021-04-16 10:53:47 +0800

    ver

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,7 +5,7 @@ Service Worker是一个标准的 web worker，浏览器用一个单独的线程�
 */
 // configuration
 const
-  version = '1618507432020',
+  version = '1618541623141',
   CACHE = version + '::ZiyiMember',
   installFilesEssential = [
     '/',
